47c9d792b684b11aa924efb2b708ef6b2880a1a6
Marko Stojkovic <markost2549@gmail.com>
2020-08-28 10:49:38 +0200

    Navigate after login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
+import { Router } from "@angular/router";
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,7 +14,10 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -44,6 +48,7 @@ export class AuthComponent implements OnInit {
       resData => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },
       errorMessage => {
         console.log(errorMessage)
