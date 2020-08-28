b79d841b5025ef24e9be37361aac2d5e346902ac
Marko Stojkovic <markost2549@gmail.com>
2020-08-28 22:24:17 +0200

    Outsourced recipe to recipeModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeService } from "./recipes/recipe.service";
-import { RecipesComponent } from "./recipes/recipes.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
@@ -21,20 +15,15 @@ import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinne
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { AlertComponent } from "./shared/alert/alert.component";
 import { PlaceholderDirective } from "./shared/placeholder/placeholder.directive";
+import { RecipesModule } from "./recipes/recipes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -46,6 +35,7 @@ import { PlaceholderDirective } from "./shared/placeholder/placeholder.directive
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
+    RecipesModule,
   ],
   providers: [ShoppingListService, RecipeService,
     {
